4dfc90a7b89b2602faa3446208c8087e84c056ca
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-04-04 15:55:59 +0600

    live link added in readme file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Getting Started with Apple Guys
 
- # You can see Live web Preview of [Apple Guys](https://github.com/facebook/create-react-app).
+ # You can see Live web Preview of [Apple Guys](https://apple-guys.netlify.app/).
 
 * The name of of the Project is Apple Guys
 * Where I created a Navbar which has 5 link With Custom link when you click A link then it'll show a green color.
@@ -8,4 +8,4 @@
 * Bellow the Home container section you can see Customer Reviews Section where I stored 3 card with customer review, customer image, customer ratings and so on. at the end of the Home section you can a Button the name of the button is See All Review. 
 * If you click See All Reviews Button it'll redirect you to the Review section. then you can see 6 card where I stored all customer Reviews.
 * The Dashboard Section where I made 4 Chart. 1. Pie Chart 2. Area Chart 3. Line Chart and 4. Composed Chart. Where I stored Investment and Review Data
-* After that I made Blogs Section Where I Answered 3 Question ans
+* After that I made Blogs Section Where I Answered 3 Question ans.
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -12,7 +12,7 @@ const Blogs = () => {
                     <p><span className='text-3xl'>Ans: </span>In any Language more important think is understand the of the words during communication. And if this a computer to computer communication that would be more critical. right? That why HTML5 privide us more semantic elements which is more understanding every single code. There is Semantic tag means. Meaning of the works and phrases. Simantic elements have a very simple meaning. It's very clear to Developer as well as Browser. Ex: In HTML we have seen div span which are not semantic element because they don't have meaning. On the other hand form, article  are semantic elements bease they have meaning.</p>
                </article>
                <article className='p-4'>
-                    <h1 className='text-2xl'>What is Block Elements and Inline Elements?</h1>
+                    <h1 className='text-2xl mb-2'>What is Block Elements and Inline Elements?</h1>
                     <p><span className='text-xl'>Ans: Block Elements</span> A Block Element is Always start with a new Line and is's stretches out from the left and right as far as it can. it hold the whole horizontal space of it's parent element. thae height is equal to the content height.</p>
                     <p><span className='text-xl'>Ans: Inline Elements</span> An Inline Element is opposite of the block level element. it does not start with new line. it takes neccessary width it onl the space bounded by the tags defining the HTML element.
                     </p>
